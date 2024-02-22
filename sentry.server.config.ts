2383a1be7bc4b5d5b7abd3993eda3096c55c7a5c
hengsokly <hengsokly23@gmail.com>
2024-02-22 14:46:27 +0700

    commend sentry logger

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,11 +5,11 @@
 import * as Sentry from "@sentry/nextjs";
 import { env } from "process";
 
-Sentry.init({
-  dsn: process.env.SENTRY_DNS,
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+// Sentry.init({
+//   dsn: process.env.SENTRY_DNS,
+//   // Adjust this value in production, or use tracesSampler for greater control
+//   tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+//   // Setting this option to true will print useful information to the console while you're setting up Sentry.
+//   debug: false,
+// });
